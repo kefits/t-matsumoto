bf582864f6b49cf6c778988b74bfe44fcaffef98
Tomoki Matsumoto <tomoki.matsumoto@bijection-space.com>
2022-07-22 14:27:07 +0900

    add

--- components/common/menu.tsx
+++ components/common/menu.tsx
@@ -11,8 +11,8 @@ import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
-import AdbIcon from "@mui/icons-material/Adb";
-import {Link} from "react-router-dom"
+// import AdbIcon from "@mui/icons-material/Adb";
+// import {Link} from "react-router-dom"
 
 const pages = ["Home", "Research", "Blog"];
 const links = ["/", "research", "https://kefism.hatenablog.com/"];
