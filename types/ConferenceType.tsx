78269a52b852976345eab02a1865a529d324f6cb
kefits <kefits@users.noreply.github.com>
2022-07-22 07:28:26 +0000

    deploy: d9475d10d483bf9aeba489d9d19672e2d5181db4

--- types/ConferenceType.tsx
+++
@@ -1,7 +0,0 @@
-export type ConferenceType = {
-  title: string;
-  // authors: Array<String>;
-  year: number;
-  conference: string;
-  url: string;
-};
--- types/PaperType.tsx
+++
@@ -1,7 +0,0 @@
-export type paperType = {
-  title: string;
-  authors: Array<String>;
-  year: number;
-  publisher: string;
-  url: string;
-};
