0b8d024407b45ac71df31ef10680f2d95633a330
Tomoki Matsumoto <tomoki.matsumoto@bijection-space.com>
2022-07-22 16:02:10 +0900

    add

--- next.config.js
+++ next.config.js
@@ -2,8 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  basePath: process.env.GITHUB_ACTIONS ? "/t-matsumoto" : "",
-  assetPrefix: process.env.NODE_ENV === "production" ? "/t-matsumoto" : "",
+  // basePath: process.env.GITHUB_ACTIONS ? "/t-matsumoto" : "",
+  // assetPrefix: process.env.NODE_ENV === "production" ? "/t-matsumoto" : "",
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
   trailingSlash: true,
 }
 
