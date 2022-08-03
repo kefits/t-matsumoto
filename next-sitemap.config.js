a0745e69dca9834ebc6d66d86c5bb0c9150c8d91
Tomoki Matsumoto <tomoki.matsumoto@bijection-space.com>
2022-08-03 17:50:18 +0900

    Add site-map.

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,9 +1,8 @@
 /** @type {import('next-sitemap').IConfig} */
 
-const config = {
+module.exports = {
   siteUrl: process.env.SITE_URL || 'https://kefits.github.io/t-matsumoto/',
   generateRobotsTxt: true, // (optional)
+  outDir: './public',
   // ...other options
 }
-
-export default config
